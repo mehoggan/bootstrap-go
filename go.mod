97997b82be05ce1b8079ea8e5cb24a6aed9871d5
Matthew Hoggan <matthew.hoggan@climate.com>
2022-10-29 06:59:04 -0500

    First initial commit.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mehoggan/bootstrap-go
+
+go 1.18
+
+require github.com/mehoggan/stringutil-go v0.0.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	stringutil "github.com/mehoggan/stringutil-go"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("Hello"))
+	fmt.Println(stringutil.ToUpper("Hello"))
+}
