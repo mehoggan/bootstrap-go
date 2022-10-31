82d2236f282162d13c58048120ad2061d9252a3f
Matthew Hoggan <matthew.hoggan@climate.com>
2022-10-31 18:43:12 -0500

    REST -- GET endpoint implemented.

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mehoggan/bootstrap-go
 
 go 1.18
 
-require github.com/mehoggan/vinyl-collection-service-go v0.0.2 // indirect
+require github.com/mehoggan/vinyl-collection-service-go v0.1.1 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 
-	vinyl_types "github.com/mehoggan/vinyl-collection-service-go/types"
+	"github.com/mehoggan/vinyl-collection-service-go/endpoints"
 )
 
 func main() {
-	var albums = []vinyl_types.Album{
-		{ID: "1",
-			Title:  "Blue Train",
-			Artist: "John Coltrane",
-			Price:  56.99},
-		{ID: "2",
-			Title:  "Jeru",
-			Artist: "Gerry Mulligan",
-			Price:  17.99},
-		{ID: "3",
-			Title:  "Sarah Vaughan and Clifford Brown",
-			Artist: "Sarah Vaughan",
-			Price:  39.99},
-	}
-	fmt.Printf("%v", albums)
+	router := gin.Default()
+	router.GET("albums", endpoints.GetAlbumsHandler)
+
+	router.Run("localhost:8080")
 }
